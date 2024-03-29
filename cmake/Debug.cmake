1e9ed9a27e72563de9f24162aa09154f3fa1276b
Jonas Printzen (codie) <jonas.printzen@gmail.com>
2024-03-29 08:09:32 +0100

    conan-runner + coverage on debug

--- cmake/Debug.cmake
+++ cmake/Debug.cmake
@@ -2,9 +2,9 @@
 function(pzen_validate target)
 
     #### TODO: Break out separate build_type ######################
-    # # Enable code coverage on provided target
-    # target_compile_options(${target} PUBLIC -std=c++20 -coverage -fno-inline )
-    # target_link_options(${target} PUBLIC -std=c++20 --coverage )
+    # Enable code coverage on provided target
+    target_compile_options(${target} PUBLIC -std=c++20 -coverage -fno-inline )
+    target_link_options(${target} PUBLIC -std=c++20 --coverage )
 
     # Regular debug-build
     target_compile_options(${target} PUBLIC -std=c++20 -fno-inline -g )
@@ -28,12 +28,12 @@ function(pzen_validate target)
     )
 
     #### TODO: Break out separate build_type ######################
-    # # Generate coverage report on provided target
-    # add_custom_target(cov_${target} ALL
-    #     COMMAND mkdir -p ${CMAKE_SOURCE_DIR}/cov
-    #     COMMAND gcovr -r ${CMAKE_SOURCE_DIR} --exclude ${CMAKE_CURRENT_SOURCE_DIR}/test --html --html-details --exclude-throw-branches --exclude-unreachable-branches --html-title "Coverage"  -o ${CMAKE_SOURCE_DIR}/cov/index.html ${target}
-    #     # COMMAND find ${CMAKE_BINARY_DIR} -name "*.gc*" -delete
-    #     DEPENDS test_${target}
-    #     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-    # )
+    # Generate coverage report on provided target
+    add_custom_target(cov_${target} ALL
+        COMMAND mkdir -p ${CMAKE_SOURCE_DIR}/cov
+        COMMAND gcovr -r ${CMAKE_SOURCE_DIR} --exclude ${CMAKE_CURRENT_SOURCE_DIR}/test --html --html-details --exclude-throw-branches --exclude-unreachable-branches --html-title "Coverage"  -o ${CMAKE_SOURCE_DIR}/cov/index.html ${target}
+        # COMMAND find ${CMAKE_BINARY_DIR} -name "*.gc*" -delete
+        DEPENDS test_${target}
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    )
 endfunction()
